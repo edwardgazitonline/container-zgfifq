a88fb62a120f4dc5a860f1cb27e34a66c15556a7
edwardgazitonline <75829088+edwardgazitonline@users.noreply.github.com>
2024-09-07 21:39:37 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component ,OnInit} from '@angular/core';
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  constructor(private heroService: HeroService) { }
+  title = 'Tour of Heroes';
+    heroes: Hero[] = [];
+
+   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
